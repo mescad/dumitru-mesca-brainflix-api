58ca9d26a5b31b18ba397c59c9c77e5466b79a47
Dumitru Mesca <dumitrumesca@gmail.com>
2023-08-09 20:36:20 +0100

    version 1 with full api endpoint

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const videos = require("./routes/videos");
+const cors= require('cors')
+
+
+
+app.use(cors())
+app.use(express.json());
+
+
+app.use(express.static('./public/'))
+
+
+
+app.use("/videos", videos);
+
+app.listen(8080, ()=>{
+    console.log("the app is listening on port 8080")
+})
+
+
---
+++ routes/videos.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const videos = require("../data/video-details.json");
+
+
+// /videos
+
+router.get("/", (req, res) => {
+  res.json(videos);
+});
+
+// /videos/:id
+router.get("/:id", (req, res) => {
+  const video = videos.find((video) => video.id == req.params.id);
+
+  if (!video) {
+    return res.status(404).json({ message: "video not found" });
+  }
+
+  res.json(video);
+});
+
+router.post("/upload", (req, res) => {
+  const { title, description } = req.body;
+
+  const newVideo = {
+    id: uuidv4(),
+    title,
+    description,
+    image:'http://localhost:8080/images/shrek.jpg',
+    views: "0",
+    likes: "0",
+    duration: "6:31",
+    channel: "Red Bull",
+
+    //
+  };
+
+  videos.push(newVideo);
+
+  res.json(newVideo);
+});
+
+module.exports = router;
